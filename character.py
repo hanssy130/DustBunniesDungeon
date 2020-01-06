fa7ff52d395559902978adcea17c64478623d57d
Hans <hanssy130@gmail.com>
2020-01-06 15:27:43 -0800

    Initial commit

---
+++ character.py
@@ -0,0 +1,13 @@
+def make_character():
+    """
+    Creates the character at the starting position.
+    """
+    return [2, 2]
+
+def make_character_health():
+    """
+    Creates the character's health.
+    """
+    return 10
+
+print(make_character_health())
